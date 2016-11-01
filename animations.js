77d8c403a4be9092d662d26559aff75ca39aaa32
Paxton Bigler <paxton.bigler@gmail.com>
2016-11-01 09:48:28 -0600

    collapsible header icons change on click

--- animations.js
+++ animations.js
@@ -7,4 +7,40 @@ $(document).ready(function(){
     $('.collapse-icon-plus-two').toggle();
     $('.collapse-icon-minus-two').toggle();
   })
+  $('.collapse-trigger-one').on('click', function(){
+    $('.down-arrow-one').toggle();
+    $('.up-arrow-one').toggle();
+  })
+  $('.collapse-trigger-two').on('click', function(){
+    $('.down-arrow-two').toggle();
+    $('.up-arrow-two').toggle();
+  })
+  $('.collapse-trigger-three').on('click', function(){
+    $('.down-arrow-three').toggle();
+    $('.up-arrow-three').toggle();
+  })
+  $('.collapse-trigger-four').on('click', function(){
+    $('.down-arrow-four').toggle();
+    $('.up-arrow-four').toggle();
+  })
+  $('.collapse-trigger-five').on('click', function(){
+    $('.down-arrow-five').toggle();
+    $('.up-arrow-five').toggle();
+  })
+  $('.collapse-trigger-six').on('click', function(){
+    $('.down-arrow-six').toggle();
+    $('.up-arrow-six').toggle();
+  })
+  $('.collapse-trigger-seven').on('click', function(){
+    $('.down-arrow-seven').toggle();
+    $('.up-arrow-seven').toggle();
+  })
+  $('.collapse-trigger-eight').on('click', function(){
+    $('.down-arrow-eight').toggle();
+    $('.up-arrow-eight').toggle();
+  })
+  $('.collapse-trigger-nine').on('click', function(){
+    $('.down-arrow-nine').toggle();
+    $('.up-arrow-nine').toggle();
+  })
 })
